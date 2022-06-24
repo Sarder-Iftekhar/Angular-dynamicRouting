2d467d4fa5a67cc0ef1d1cafb205cdcdea949c82
Sarder-Iftekhar <iftekhar.sarder@gmail.com>
2022-06-24 22:12:28 +0600

    first commit,dynamic routing basic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:HomeComponent,
+    path:''
+  },
+  {
+    component:AboutComponent,
+    path:'about'
+  },
+  {
+    component:UserComponent,
+    path:'user/:id'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
